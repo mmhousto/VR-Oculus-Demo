ed6def396508128bd269fb3f3c09c22555d81b02
mmhousto <mmhouston6@live.com>
2021-12-05 20:28:08 -0600

    Replaced hands with balls.
    Buckets fly back now. Score goes up. Hand were not syncing correctly after update, switched to sphere's as hands for placeholder.

--- Assets/Scripts/HitBucket.cs
+++ Assets/Scripts/HitBucket.cs
@@ -5,23 +5,32 @@ using UnityEngine;
 public class HitBucket : MonoBehaviour
 {
 
+    private bool hasHit;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        hasHit = false;
     }
 
 
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.tag == "Enemy")
+        if(collision.gameObject.CompareTag("Enemy") && !hasHit)
         {
+            hasHit = true;
             ScoreManager.AddScore();
             collision.gameObject.GetComponent<BucketFollow>().Stop();
-            collision.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * 20f + Vector3.forward * -50f);
+            //collision.gameObject.GetComponent<Rigidbody>().AddExplosionForce(500f, collision.transform.position, 2f, 500f);
+            collision.gameObject.GetComponent<Rigidbody>().AddForce(-collision.transform.forward * 20f + Vector3.up * 50f, ForceMode.Impulse);
             collision.gameObject.GetComponent<AudioSource>().Play();
-            Destroy(collision.gameObject);
-            
+            Destroy(collision.gameObject, 1f);
+            Invoke(nameof(ResetBool), 0.2f);
         }
+    }
+
+    private void ResetBool()
+    {
+        hasHit = false;
     }
 }
